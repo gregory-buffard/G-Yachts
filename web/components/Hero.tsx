003a5f3f81143481ee9f74c1564108d52cf7857b
Maks <petriv050711@gmail.com>
2024-04-23 15:46:37 +0200

    Creating text sliding animation on Home Page

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Hero = () => {
     return (
-            <div className="bg-hero-bg-image bg-center h-screen w-screen overflow-hidden"></div>
+        <div className="bg-hero-bg-image bg-center h-screen w-screen justify-center overflow-hidden font-slick text-5xl md:text-4xl text-center">Superyachts<span className="font-classic inline-flex flex-col">
+            <ul className="block text-left leading-tight [&_li]:block">
+                <li>SALES</li>
+                <li>CHARTER</li>
+                <li>MANAGEMENT</li>
+                <li>NEW CONSTRUCTIONS</li>
+            </ul>
+        </span></div>
     );
 }
 
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -11,6 +11,31 @@ const config: Config = {
       classic: ['"Beausite Classic"', "sans-serif"],
       slick: ['"Beausite Slick"', "serif"],
     },
+    animation: {
+      'text-slide': 'text-slide 12.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+    },
+    keyframes: {
+      'text-slide': {
+        '0%, 16%': {
+          transform: 'translateY(0%)',
+        },
+        '20%, 36%': {
+          transform: 'translateY(-16.66%)',
+        },
+        '40%, 56%': {
+          transform: 'translateY(-33.33%)',
+        },
+        '60%, 76%': {
+          transform: 'translateY(-50%)',
+        },
+        '80%, 96%': {
+          transform: 'translateY(-66.66%)',
+        },
+        '100%': {
+          transform: 'translateY(-83.33%)',
+        },
+      },
+    },
     backgroundImage: {
       "hero-bg-image": "url('../public/imagery/6.png')",
     },
