904dfcd24c2e61f13a8a1ba7890a096a0d658cd4
Maks <petriv050711@gmail.com>
2024-04-24 15:44:33 +0200

    Resized images

--- web/components/OurServices.tsx
+++ web/components/OurServices.tsx
@@ -5,16 +5,18 @@ const OurServices = () => {
   const t = useTranslations("ourServices");
   return (
     <div className="bg-rock-100 bg-cover h-full">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container flex flex-col items-start mx-auto px-0 py-16">
         <h2 className="text-4xl font-slick text-left mb-8">{t("title")}</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <div className="flex flex-col ">
+          <div className="flex flex-col">
             <img
               src={"/imagery/penne.jpg"}
               alt="Sales"
-              className="mb-4 shadow-lg"
+              className="mb-4 shadow-lg w-full h-full object-center"
             />
-            <h3 className="text-xl font-classic text-left">{t("sales")}</h3>
+            <h3 className="text-xl font-classic text-left relative">
+              {t("sales")}
+            </h3>
             <p className={"font-slick text-2xl"}>{t("sales-p")}</p>
           </div>
 
@@ -22,7 +24,7 @@ const OurServices = () => {
             <img
               src="/imagery/IMG_8259.JPG"
               alt="Charter"
-              className="mb-4 shadow-lg"
+              className="mb-4 shadow-lg w-full h-full"
             />
             <h3 className="text-xl font-classic text-left">{t("charter")}</h3>
             <p className={"font-slick text-2xl"}>{t("charter-p")}</p>
@@ -32,7 +34,7 @@ const OurServices = () => {
             <img
               src="/imagery/5.png"
               alt="Management"
-              className="mb-4 shadow-lg"
+              className="mb-4 shadow-lg w-full h-full"
             />
             <h3 className="text-xl font-classic text-left">
               {t("management")}
@@ -44,7 +46,7 @@ const OurServices = () => {
             <img
               src="/imagery/NEW CONSTRUCTION.jpeg"
               alt="New Construction"
-              className="mb-4 shadow-lg"
+              className="mb-4 shadow-lg w-full h-full object-cover"
             />
             <h3 className="text-xl font-classic">{t("construction")}</h3>
             <p className={"font-slick text-2xl"}>{t("construction-p")}</p>
