d393684357c927a9d4827f4997214dfc00e9f93e
Maks <petriv050711@gmail.com>
2024-04-27 21:58:26 +0200

    Responsivity finished

--- web/components/index/hero.tsx
+++ web/components/index/hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
   return (
     <div className="md:bg-left-top bg-center bg-cover bg-hero h-screen w-full overflow-hidden">
       <div className="grid grid-rows-6 h-full">
-        <div className="row-span-4 flex flex-col items-start justify-end md:ml-28 ml-16 mb-28">
-          <h2 className="font-classic text-white md:text-xl text-base">
+        <div className="row-span-4 flex flex-col items-start justify-end md:ml-28 ml-4 mb-28">
+          <h2 className="font-classic text-white md:text-xl text-sm py-2">
             {t("subtitle")}
           </h2>
-          <div className="font-slick text-5xl md:text-7xl text-white [text-wrap:balance] bg-clip-text">
+          <div className="font-slick text-4xl md:text-7xl text-white [text-wrap:balance] bg-clip-text">
             Superyachts{" "}
-            <span className="font-classic inline-flex flex-col h-[calc(theme(fontSize.5xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.6xl)*theme(lineHeight.tight))] overflow-hidden">
+            <span className="font-classic inline-flex flex-col h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.6xl)*theme(lineHeight.tight))] overflow-hidden">
               <ul className="block animate-text-slide-4 text-left leading-tight [&_li]:block">
                 <li>{t("sales")}</li>
                 <li>CHARTER</li>
@@ -24,12 +24,12 @@ const Hero = () => {
             </span>
           </div>
         </div>
-        <div className="row-span-2 flex flex-col items-end justify-center h-full">
-          <div className={"flex flex-col mt-24 -mr-20 md:ml-10 ml-20"}>
-            <p className="text-white font-classic md:w-8/12 w-5/6">
-              {t("undertitle")}
+        <div className="row-span-2 flex flex-col place-items-end justify-center">
+          <div className={"flex flex-col md:mr-20 mx-4 mt-20"}>
+            <p className="text-white font-classic text-base">
+              {t.rich("undertitle", { br: () => <br /> })}
             </p>
-            <div className={"flex flex-row gap-2 py-5 float-left"}>
+            <div className={"flex flex-row py-5 gap-4"}>
               <button
                 type={"button"}
                 className="glass-button glass-button-light"
--- web/components/index/services.tsx
+++ web/components/index/services.tsx
@@ -10,42 +10,62 @@ const Services = () => {
   const t = useTranslations("index.services");
 
   return (
-    <div className="bg-rock-100 bg-cover h-full">
-      <div className="container flex flex-col items-start mx-auto px-0 py-16">
+    <div className="bg-rock-100 w-full h-full">
+      <div className="flex flex-col items-start mx-auto md:px-10 px-3 py-16">
         <h2 className="text-4xl font-slick text-left mb-8">{t("title")}</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 lg:grid-cols-4 md:gap-6 gap-2">
           <div className="flex flex-col ">
-            <Image src={sales} alt="Sales" className="mb-4 shadow-lg" />
-            <h3 className="text-xl font-classic text-left">{t("sales")}</h3>
-            <p className={"font-slick text-2xl"}>{t("sales-p")}</p>
+            <Image
+              src={sales}
+              alt="Sales"
+              className="mb-4 shadow-lg lg:h-[65vh] h-[35vh]"
+            />
+            <h3 className="md:text-xl text-lg font-classic text-left">
+              {t("sales")}
+            </h3>
+            <p className={"font-slick md:text-2xl text-base"}>{t("sales-p")}</p>
           </div>
 
           <div className="flex flex-col">
-            <Image src={charter} alt="Charter" className="mb-4 shadow-lg" />
-            <h3 className="text-xl font-classic text-left">{t("charter")}</h3>
-            <p className={"font-slick text-2xl"}>{t("charter-p")}</p>
+            <Image
+              src={charter}
+              alt="Charter"
+              className="mb-4 shadow-lg lg:h-[65vh] h-[35vh]"
+            />
+            <h3 className="md:text-xl text-lg font-classic text-left">
+              {t("charter")}
+            </h3>
+            <p className={"font-slick md:text-2xl text-base"}>
+              {t("charter-p")}
+            </p>
           </div>
 
           <div className="flex flex-col">
             <Image
               src={management}
               alt="Management"
-              className="mb-4 shadow-lg w-full h-full"
+              className="mb-4 shadow-lg lg:h-[65vh] h-[35vh]"
             />
-            <h3 className="text-xl font-classic text-left">
+            <h3 className="md:text-xl text-lg font-classic text-left">
               {t("management")}
             </h3>
-            <p className={"font-slick text-2xl"}>{t("management-p")}</p>
+            <p className={"font-slick md:text-2xl text-base"}>
+              {t("management-p")}
+            </p>
           </div>
 
           <div className="flex flex-col">
             <Image
               src={construction}
               alt="New Construction"
-              className="mb-4 shadow-lg w-full h-full object-cover"
+              className="mb-4 shadow-lg lg:h-[65vh] h-[35vh] object-cover"
             />
-            <h3 className="text-xl font-classic">{t("construction")}</h3>
-            <p className={"font-slick text-2xl"}>{t("construction-p")}</p>
+            <h3 className="md:text-xl text-lg font-classic">
+              {t("construction")}
+            </h3>
+            <p className={"font-slick md:text-2xl text-base"}>
+              {t("construction-p")}
+            </p>
           </div>
         </div>
       </div>
