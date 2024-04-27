b591fe6ed183ee061a07bb9d58dc1d7b0251fa46
Maks <petriv050711@gmail.com>
2024-04-27 23:41:42 +0200

    Optimisation of Learn More section

--- web/app/[locale]/page.tsx
+++ web/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import Bar from "@/components/nav/bar";
 import Hero from "@/components/index/hero";
 import { useTranslations } from "next-intl";
 import dynamic from "next/dynamic";
+import React from "react";
 
 const Services = dynamic(() => import("@/components/index/services"));
 const WorkingTogether = dynamic(() => import("@/components/index/working"));
@@ -14,19 +15,13 @@ const Home = () => {
     <main className="w-full flex flex-col justify-start items-center">
       <Bar dynamicColor={100} />
       <Hero />
-      <section className="bg-white py-32 flex flex-col leading-relaxed justify-center items-start containerize">
+      <section className="bg-white py-28 flex flex-col leading-relaxed justify-center items-start containerize">
         <div className="flex flex-col font-slick">
           <h1 className={"md:text-4xl text-2xl leading-normal"}>
-            {t("learn.title")}
-          </h1>
-          <h1 className={"md:text-4xl text-2xl leading-normal"}>
-            {t("learn.title1")}
-          </h1>
-          <h1 className={"md:text-4xl text-2xl leading-normal"}>
-            {t("learn.title2")}
+            {t.rich("learn.title", { br: () => <br /> })}
           </h1>
         </div>
-        <button type={"button"} className="glass-button glass-button-dark">
+        <button type={"button"} className="glass-button glass-button-dark mt-8">
           {t("learn.CTA")}
         </button>
       </section>
