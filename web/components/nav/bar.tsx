11cd0a54971c220e445f5e4e96d305f1b5db84d3
Gregory Buffard <gregory442005@gmail.com>
2024-05-02 19:41:17 +0200

    Final UI bugfixes
    The Bar tinted Bar menu now flows over content instead underneath and the Burger menu icon's gap is back to normal

--- web/components/nav/bar.tsx
+++ web/components/nav/bar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import View from "@/components/nav/view";
 import Logo from "@/public/logo/logo";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 import { useView } from "@/app/store";
+import { Link } from "@/navigation";
 
 const Bar = ({ dynamicColor }: { dynamicColor: number }) => {
   const t = useTranslations("bar"),
@@ -27,17 +27,24 @@ const Bar = ({ dynamicColor }: { dynamicColor: number }) => {
   return (
     <>
       <div
-        className={`fixed w-full h-[16vh] top-0 z-0 bg-gradient-to-b from-white from-10% transition-opacity duration-200 ease-in-out ${isScrolled ? "opacity-100" : "opacity-0"}`}
+        className={`fixed w-full h-[16vh] top-0 z-10 bg-gradient-to-b from-white from-10% transition-opacity duration-200 ease-in-out ${isScrolled ? "opacity-100" : "opacity-0"}`}
       />
-      <Logo
-        className={`fixed top-0 lg:h-[2.5vw] h-[4vh] lg:z-20 lg:my-[4vh] my-[2vh] z-10 transition-[fill] duration-200 ease-in-out ${isScrolled ? "fill-black" : "fill-white"}`}
-      />
+      <Link
+        href={"/"}
+        className={
+          "fixed top-0 lg:h-[2.5vw] h-[4vh] lg:z-20 lg:my-[4vh] my-[2vh] z-10"
+        }
+      >
+        <Logo
+          className={`w-full h-full transition-[fill] duration-200 ease-in-out ${isScrolled ? "fill-black" : "fill-white"}`}
+        />
+      </Link>
       <section
         className={`fixed top-0 flex justify-between items-center lg:py-[4vh] py-[2vh] containerize z-10`}
       >
         <button
           onClick={() => openView("navigation")}
-          className={`center flex-col lg:w-[2vw] w-[4vw] gap-[0.5vh] lg:hover:gap-[0.75vh] lg:transition-[gap] duration-200 ease-in-out ${isScrolled ? "fill-black" : "fill-white"}`}
+          className={`center flex-col lg:w-[2vw] w-[4vw] group lg:hover:gap-[0.75vh] ${isScrolled ? "fill-black" : "fill-white"}`}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +56,9 @@ const Bar = ({ dynamicColor }: { dynamicColor: number }) => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 8"
-            className={"burger-bar"}
+            className={
+              "burger-bar my-[0.30rem] group-hover:my-[0.48rem] lg:transition-[margin] duration-200 ease-in-out"
+            }
           >
             <rect width="128" height="8" rx="4" />
           </svg>
