25932c8e9055b4608587e49846b061c590daa190
Maksym Petriv <78928341+MaxiPet007@users.noreply.github.com>
2024-05-31 08:49:50 +0200

    fuck my life

--- web/components/company/hero.tsx
+++ web/components/company/hero.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { useTranslations } from "next-intl";
-import hero from "@/public/imagery/optimized/index/hero.webp";
+import herox from "@/public/imagery/optimized/index/herox.webp";
 import Image from "next/image";
 
 const Hero = () => {
   const t = useTranslations("company.hero");
   return (
-    <div className="h-screen w-full overflow-hidden">
+    <div className="h-full w-full overflow-hidden">
       <div className="grid grid-cols-2">
-        <div className={"bg-rock-100"}>
-            
+        <div className={"bg-rock-100 "}>
+            <div>
+                <h1 className={"font-classic font-light text-3xl md:text-6xl text-black"}>
+                    {t("subtitle")}
+                </h1>
+            </div>
         </div>
-        <Image src={hero} alt={"Yacht"} className={"bg-contain h-screen"}/>
+        <Image src={herox} alt={"Yacht"} className={"object-cover h-screen"}/>
       </div>
     </div>
     );
