8513dbeed79f47c29c239bc9cefa4e4f4983ab6b
kuba <dornicakkuba@gmail.com>
2024-06-02 17:31:00 +0200

    feat: 🎸 Working mobile navbar

--- admin/components/nav.tsx
+++ admin/components/nav.tsx
@@ -12,29 +12,64 @@ import { FaArrowsSpin } from "react-icons/fa6";
 import { FaNewspaper } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import { RiMegaphoneFill } from "react-icons/ri";
-import { GiPalmTree } from "react-icons/gi";
+import { GiHamburgerMenu, GiPalmTree } from "react-icons/gi";
+import { useState } from "react";
 
-const navItems: { icon: IconType; id: NavActive; }[] = [
-    { icon: FaHome, id: "dashboard" },
-    { icon: IoMdBoat, id: "yachts" },
-    { icon: FaArrowsSpin, id: "charters" },
-    { icon: GiPalmTree, id: "destinations" },
-    { icon: FaPlus, id: "new" },
-    { icon: RiMegaphoneFill, id: "newsletter" },
-    { icon: FaNewspaper, id: "article" },
-    { icon: FaMessage, id: "messages" },
+const navItems: { icon: IconType; id: NavActive; title: string }[] = [
+    {
+        icon: FaHome,
+        id: "dashboard",
+        title: "Dashboard"
+    },
+    {
+        icon: IoMdBoat,
+        id: "yachts",
+        title: "Yachts"
+    },
+    {
+        icon: FaArrowsSpin,
+        id: "charters",
+        title: "Charters"
+    },
+    {
+        icon: GiPalmTree,
+        id: "destinations",
+        title: "Destinations",
+    },
+    {
+        icon: FaPlus,
+        id: "new",
+        title: "New"
+    },
+    {
+        icon: RiMegaphoneFill,
+        id: "newsletter",
+        title: "Newsletters"
+    },
+    {
+        icon: FaNewspaper,
+        id: "article",
+        title: "Articles"
+    },
+    {
+        icon: FaMessage,
+        id: "messages",
+        title: "Customer messages"
+    },
 ];
 
 const Selector = ({
     active,
     id,
     Icon,
-    onSetActive
+    onSetActive,
+    title,
 }: {
     active: NavActive;
-    id: NavActive,
-    Icon: IconType,
+    id: NavActive;
+    Icon: IconType;
     onSetActive: (id: NavActive) => void;
+    title?: string;
 }) => {
     return (
         <Button
@@ -44,16 +79,29 @@ const Selector = ({
                 onSetActive(id);
             }}
             className={twMerge([
-                "size-[4vh] fill-neutral-100 text-neutral-800 p-[0.5vh] rounded-2xl",
-                active === id ? "bg-primary-500" : "bg-neutral-400/35"
+                "lg:size-[4vh] w-full text-left fill-neutral-100 align-middle text-neutral-800 px-5 rounded-2xl",
+                active === id ? "bg-primary-500" : "bg-neutral-400/35",
             ])}
         >
-            {<Icon
-                className={twMerge([
-                    active === id ? "text-white" : "text-neutral-700"
-                ])}
-                fontSize={25}
-            />}
+            <div className="flex flex-row gap-2">
+                <Icon
+                    className={twMerge([
+                        active === id ? "text-white" : "text-neutral-700",
+                    ])}
+                    fontSize={25}
+                />
+                {
+                    title && (
+                        <span
+                            className={twMerge([
+                                "text-sm h-fit",
+                                active === id ? "text-white" : "text-neutral-700",
+                            ])}
+                        >
+                            {title}
+                        </span>
+                    )}
+            </div>
         </Button>
     );
 };
@@ -61,28 +109,65 @@ const Selector = ({
 const Nav = () => {
     const { active, setActive } = useViewContext();
 
+    const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+    const toggleMobileOpen = () => setMobileOpen(!mobileOpen);
+
     return (
-        <nav
-            className={
-                "fixed bottom-[4vh] z-50 w-full flex justify-center items-center"
-            }
-        >
-            <div
+        <>
+            <nav
                 className={
-                    "bg-neutral-300/50 border-neutral-300/25 border-[0.25vh] backdrop-blur-2xl px-[2vw] py-[1vh] rounded-3xl flex justify-center items-center gap-8"
+                    "fixed bottom-[4vh] z-50 w-full flex justify-center items-center"
                 }
             >
-                {navItems.map(item => (
-                    <Selector
-                        key={item.id}
-                        active={active}
-                        onSetActive={setActive}
-                        id={item.id}
-                        Icon={item.icon}
-                    />
-                ))}
-            </div>
-        </nav>
+                <div className="lg:hidden flex justify-between items-center w-full px-4">
+                    <Button isIconOnly={true} variant={"shadow"} onPress={toggleMobileOpen}>
+                        <GiHamburgerMenu fontSize={25} />
+                    </Button>
+                </div>
+
+                <div className={twMerge([
+                    "lg:hidden fixed top-0 right-0 w-[250px] h-screen z-[1000] bg-white cursor-pointer transition-transform duration-300 ease-in-out",
+                    mobileOpen ? "translate-x-0" : "translate-x-full"
+                ])}>
+                    <div className="flex flex-col items-start p-4">
+                        <Button isIconOnly={true} variant={"shadow"} onPress={toggleMobileOpen}>
+                            <GiHamburgerMenu fontSize={25} />
+                        </Button>
+                        <div className="mt-8 flex flex-col gap-4">
+                            {navItems.map(item => (
+                                <Selector
+                                    key={item.id}
+                                    active={active}
+                                    onSetActive={(id) => {
+                                        setActive(id);
+                                        toggleMobileOpen();
+                                    }}
+                                    id={item.id}
+                                    Icon={item.icon}
+                                    title={item.title}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    className={
+                        "bg-neutral-300/50 lg:flex hidden border-neutral-300/25 border-[0.25vh] backdrop-blur-2xl px-[2vw] py-[1vh] rounded-3xl justify-center items-center gap-8"
+                    }
+                >
+                    {navItems.map(item => (
+                        <Selector
+                            key={item.id}
+                            active={active}
+                            onSetActive={setActive}
+                            id={item.id}
+                            Icon={item.icon}
+                        />
+                    ))}
+                </div>
+            </nav>
+        </>
     );
 };
 
