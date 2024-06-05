f320da497935b5d0fd41052e3c1dadc7ca21beee
Maksym Petriv <78928341+MaxiPet007@users.noreply.github.com>
2024-06-05 21:51:47 +0200

    Page recruitment

--- web/app/[locale]/recruitment/page.tsx
+++ web/app/[locale]/recruitment/page.tsx
@@ -1,5 +1,6 @@
 import Bar from "@/components/nav/bar";
 import dynamic from "next/dynamic";
+import Hero from "@/components/recruitment/hero";
 
 const View = dynamic(() => import("@/components/view"));
 const Footer = dynamic(() => import("@/components/footer"));
@@ -10,6 +11,7 @@ const Recruitment = () => {
     <main className="w-full flex flex-col justify-start items-center">
       <Bar dynamicColor={100} />
       <View />
+      <Hero />
       <Newsletter />
       <Footer />
     </main>
