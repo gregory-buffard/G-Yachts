994637fde0fb62cfd3f655bbd9a8dee63633a767
Gregory Buffard <gregory442005@gmail.com>
2024-06-06 15:16:48 +0200

    kms
    i want to kms

--- web/app/[locale]/charter/[id]/page.tsx
+++ web/app/[locale]/charter/[id]/page.tsx
@@ -4,19 +4,27 @@ import { fetchCharter } from "@/actions/charter";
 import { CharterProvider } from "@/context/charter";
 import Hero from "@/components/charter/hero";
 import Details from "@/components/charter/details/details";
+import Brokerino from "@/components/yacht/brokerino";
+import { fetchBrokerino } from "@/actions/brokerino";
 
 const View = dynamic(() => import("@/components/view"));
 const Newsletter = dynamic(() => import("@/components/newsletter"));
 const Footer = dynamic(() => import("@/components/footer"));
 
 const Charter = async ({ params }: { params: { id: string } }) => {
+  const yacht = await fetchCharter(params.id);
+
   return (
-    <CharterProvider charter={await fetchCharter(params.id)}>
+    <CharterProvider charter={yacht}>
       <main className="w-full flex flex-col justify-start items-center">
         <Bar dynamicColor={100} />
         <View />
         <Hero />
-        <Details />
+        {yacht && (
+          <Details>
+            <Brokerino brokerino={await fetchBrokerino(yacht.brokerEmail)} />
+          </Details>
+        )}
         <Newsletter />
         <Footer />
       </main>
