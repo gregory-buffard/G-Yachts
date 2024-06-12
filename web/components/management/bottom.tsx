bdd7b8b648abe0de16a8252a9f6ccdfc706d96ee
Gregory Buffard <gregory442005@gmail.com>
2024-06-12 14:07:36 +0200

    Added missing Contact button
    Management page, bottom component features a new Contact button.

--- web/components/management/bottom.tsx
+++ web/components/management/bottom.tsx
@@ -1,24 +1,37 @@
+"use client";
+
 import { useTranslations } from "next-intl";
-import React from "react";
+import { useViewContext } from "@/context/view";
 
 const Bottom = () => {
-  const t = useTranslations("management.bottom");
+  const t = useTranslations("management.bottom"),
+    { openView } = useViewContext();
   return (
-    <section className={"w-full h-[36dvh] md:h-[46dvh] relative overflow-hidden"}>
-        <div className={"text-center"}>
-            <video
-                src="/imagery/optimized/management/bottomVideo.mp4"
-                autoPlay
-                loop
-                muted
-                preload={"none"}
-                className="w-full absolute -z-10 h-full object-cover object-center"
-                playsInline
-            />
-            <h1 className={"text-white py-20"}>{t.rich("title", {
-                classic: (chunk) => <span className={"classic"}>{chunk}</span>,
-            })}</h1>
-        </div>
+    <section
+      className={"w-full h-[36dvh] md:h-[46dvh] relative overflow-hidden"}
+    >
+      <div className={"text-center"}>
+        <video
+          src="/imagery/optimized/management/bottomVideo.mp4"
+          autoPlay
+          loop
+          muted
+          preload={"none"}
+          className="w-full absolute -z-10 h-full object-cover object-center"
+          playsInline
+        />
+        <h1 className={"text-white py-20"}>
+          {t.rich("title", {
+            classic: (chunk) => <span className={"classic"}>{chunk}</span>,
+          })}
+        </h1>
+        <button
+          className={"glass-button glass-button-light"}
+          onClick={() => openView("contact")}
+        >
+          {t("CTA")}
+        </button>
+      </div>
     </section>
   );
 };
