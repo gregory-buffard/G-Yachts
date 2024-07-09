92a197c0545969ba8f7eaf3319ee8e4e84eaada2
Maksym Petriv <78928341+MaxiPet007@users.noreply.github.com>
2024-07-10 00:08:04 +0200

    Partners v 1

--- web/app/[locale]/partners/page.tsx
+++ web/app/[locale]/partners/page.tsx
@@ -1,2 +1,21 @@
 import Bar from "@/components/nav/bar";
 import dynamic from "next/dynamic";
+import Hero from "@/components/partners/hero";
+
+const View = dynamic(() => import("@/components/view"));
+const Newsletter = dynamic(() => import("@/components/newsletter"));
+const Footer = dynamic(() => import("@/components/footer"));
+
+const Partners = () => {
+  return (
+    <main className="w-full flex flex-col justify-start items-center">
+      <Bar dynamicColor={100} />
+      <View />
+      <Hero />
+      <Newsletter />
+      <Footer />
+    </main>
+  );
+};
+
+export default Partners;
