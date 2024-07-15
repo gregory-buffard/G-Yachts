9049f4bbc5513ae9257dea41f0c1f43bbffa8672
Maksym Petriv <78928341+MaxiPet007@users.noreply.github.com>
2024-07-15 18:16:40 +0200

    15 july

--- web/components/company/team.tsx
+++ web/components/company/team.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import React from "react";
 import { useTranslations } from "next-intl";
+import Brokerino from "@/components/company/brokerino";
 
 const Team = () => {
-  const t = useTranslations("company.team");
-  return <div>dsa</div>;
+  const t = useTranslations("company");
+  return <section className={"w-full h-full bg-rock-100 overflow-hidden"}>
+    <h1 className={"px-[10vw]"}>{t.rich("team", {
+      classic: (chunks) => (
+          <span className={"font-classic font-normal uppercase"}>
+                  {chunks}
+                </span>
+      ),
+    })}</h1>
+    <Brokerino brokerino={Brokerino}/>
+  </section>;
 };
 
 export default Team;
