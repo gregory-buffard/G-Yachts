8553f6a9d173a6d7dfe946480091d26f1aad09b5
Maks <petriv050711@gmail.com>
2024-07-18 18:19:09 +0200

    FIx

--- web/components/company/team.tsx
+++ web/components/company/team.tsx
@@ -1,21 +1,21 @@
-"use client";
-
 import React from "react";
 import { useTranslations } from "next-intl";
-import Brokerino from "@/components/company/brokerino";
 
 const Team = () => {
   const t = useTranslations("company");
-  return <section className={"w-full h-full bg-rock-100 overflow-hidden"}>
-    <h1 className={"px-[10vw]"}>{t.rich("team", {
-      classic: (chunks) => (
-          <span className={"font-classic font-normal uppercase"}>
-                  {chunks}
-                </span>
-      ),
-    })}</h1>
-    <Brokerino brokerino={Brokerino}/>
-  </section>;
+  return (
+    <section className={"w-full h-full bg-rock-100 overflow-hidden"}>
+      <h1 className={"px-[10vw]"}>
+        {t.rich("team", {
+          classic: (chunks) => (
+            <span className={"font-classic font-normal uppercase"}>
+              {chunks}
+            </span>
+          ),
+        })}
+      </h1>
+    </section>
+  );
 };
 
 export default Team;
