f442417cf94a6e9c7f4b8c2fe546fb563e4bcf0e
Gregory Buffard <gregory442005@gmail.com>
2024-07-28 00:15:50 +0200

    More patches on the way

---
+++ web/utils/contact.ts
@@ -0,0 +1,43 @@
+import { usePathname } from "@/navigation";
+
+export const usePath = (): string => {
+  const pathname = usePathname();
+  switch (pathname) {
+    case "/":
+      return "Main Page";
+    case "/sales":
+      return "Sales Page";
+    case "/sales/[id]":
+      return "Yacht Page";
+    case "/charter/[id]":
+      return "Yacht for Charter Page";
+    case "/charters":
+      return "Charters Page";
+    case "/new-constructions":
+      return "New Constructions Page";
+    case "/new-constructions/[id]":
+      return "New Construction Page";
+    case "/management":
+      return "Management Page";
+    case "/company":
+      return "Company Page";
+    case "/partners":
+      return "Partners Page";
+    case "/news":
+      return "News Page";
+    case "/news/[id]":
+      return "News Article Page";
+    case "/events":
+      return "Events Page";
+    case "/events/[id]":
+      return "Event Page";
+    case "/recruitment":
+      return "Recruitment Page";
+    case "/destinations":
+      return "Destinations Page";
+    case "/destinations/[id]":
+      return "Destination Page";
+    default:
+      return "Unknown Page";
+  }
+};
